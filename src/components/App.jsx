c8dbcbe7471ba41624e34e2f3534f2c6bd3beb37
Serhiy Shvayka <sergiyshvayka@gmail.com>
2022-09-20 10:49:08 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,78 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+import React, { Component } from 'react';
+import { Filter } from './Filter/Filter';
+import s from './App.module.css';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Container from './Container/Container';
+
+export class App extends Component {
+  static propTypes = {
+    filter: PropTypes.string,
+    contacts: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+      })
+    ),
+  };
+
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = (name, number) => {
+    if (this.state.contacts.some(contact => contact.name === name)) {
+      return alert(`${name} is already in contacts`);
+    }
+
+    const newContact = {
+      name,
+      number,
+      id: nanoid(),
+    };
+
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  removeContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    const visibleContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
+    return (
+      <Container>
+        <h1 className={s.mainHeader}>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+
+        <section>
+          <h2 className={s.addHeader}>Contacts</h2>
+          <Filter value={filter} onChange={this.changeFilter} />
+
+          <ul className={s.list}>
+            <ContactList data={visibleContacts} onClick={this.removeContact} />
+          </ul>
+        </section>
+      </Container>
+    );
+  }
+}
+
+export default App;
